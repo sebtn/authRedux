e78fac7e6ba653b49e011f5a0076f138c6a87d1f
sebtn <sdebedout@gmail.com>
2017-07-14 13:07:01 -0400

    Connected to redux store

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-### Redux, React and Karma boiler plate 
+### Redux, React and Karma Authredux
 -----
 + Uses webpack and express to serve devEnv
 + Node js and react for UI designing
 + Bootstrapped for components styling 
 + Redux boiler plate
 + Karma installed and optimized for fast unit testing  
++ Higher ordrer components utilization in app
 
 `` Author = () => { Sebtn } ``
--- app/app.js
+++ app/app.js
@@ -2,10 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import {Route, Router, IndexRoute, hashHistory, Link} from 'react-router'
+import {Provider} from 'react-redux'
+import ReduxPromise from 'redux-promise'
+import {createStore, applyMiddleware, compose} from 'redux'
 
 import  '././styles/app.scss' 
+import reducers from './reducers'
 
+const createStoreMiddleware =applyMiddleware(ReduxPromise)(createStore)
+const devExtension = compose(window.devToolsExtension ? window.devToolsExtension() : f => f )
 
 'use strict'
-
-ReactDOM.render(<h3>Using Redux</h3>, document.getElementById('root'))
+ReactDOM.render(
+  <Provider>
+    <h3 store={createStoreMiddleware(reducers, devExtension)} >
+      Using Redux
+    </h3>
+  </Provider>
+  ,document.getElementById('root'))
